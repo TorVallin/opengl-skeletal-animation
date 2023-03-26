14b4563e2c5e2a54b6d96f39203e656989c6c2e9
Tor Vallin <c18tvn@cs.umu.se>
2023-03-26 13:26:28 +0200

    Separate implementation & declarations

--- src/models/AnimatedModelLoader.h
+++ src/models/AnimatedModelLoader.h
@@ -40,167 +40,23 @@ static const aiScene *setup_scene(const std::string &path) {
 
 class AnimatedModelLoader {
  public:
-
   [[nodiscard]] static std::optional<Model> load_model(const std::string &model_path,
-													   const std::string &animation_path) {
-	auto model_scene = setup_scene(model_path);
-
-	Model model{};
-	load_node(model, model_scene, model_scene->mRootNode);
-
-	auto animation_scene = setup_scene(animation_path);
-	auto animation = animation_scene->mAnimations[1];
-	model.ticks_per_second = animation->mTicksPerSecond;
-	model.animation_duration = animation->mDuration;
-	load_node_animations(animation_scene, animation_scene->mRootNode, model, -1);
-	load_bones(model, animation);
-
-	return model;
-  }
+													   const std::string &animation_path);
  private:
-
-  static void load_node(Model &model, const aiScene *scene, const aiNode *node) {
-	for (unsigned int i = 0; i < node->mNumMeshes; i++) {
-	  // The node object only contains indices to index the actual objects in the scene.
-	  // The scene contains all the data, node is just to keep stuff organized (like relations between nodes).
-	  aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
-	  model.mesh_list.push_back(load_mesh(scene, mesh, model));
-	}
-
-	for (unsigned int i = 0; i < node->mNumChildren; i++) {
-	  load_node(model, scene, node->mChildren[i]);
-	}
-  }
-
+  static void load_node(Model &model, const aiScene *scene, const aiNode *node);
   static void load_node_animations(const aiScene *scene,
 								   const aiNode *node,
 								   Model &model,
-								   int parent_index) {
-	model.node_list.emplace_back(node->mName.data,
-								 Conversions::convertAssimpMat4ToGLM(node->mTransformation),
-								 parent_index);
-
-	// The current node is of course a parent to its children, meaning its ID will be the childrens' parent ID
-	// in the next recursive call.
-	// After we've processed all of the meshes (if any) we then recursively process each of the children nodes
-	int this_node_index = (int)(model.node_list.size() - 1);
-	for (unsigned int i = 0; i < node->mNumChildren; i++) {
-	  load_node_animations(scene, node->mChildren[i], model, this_node_index);
-	}
-  }
+								   int parent_index);
 
   [[nodiscard]] static Mesh load_mesh(const aiScene *,
 									  const aiMesh *mesh,
 									  Model &model
-  ) {
-	std::vector<AnimatedVertex> all_vertices;
-	std::vector<unsigned int> all_indices;
+  );
+  static void load_vertex_bone_weights(const aiMesh *mesh, std::vector<AnimatedVertex> &vertices, Model &model);
+  static void load_bones(Model &model, const aiAnimation *animation);
 
-	for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
-	  AnimatedVertex vert{};
-	  vert.pos = glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
-	  all_vertices.push_back(vert);
-	}
-
-	for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
-	  for (unsigned int j = 0; j < mesh->mFaces[i].mNumIndices; j++) {
-		all_indices.push_back(mesh->mFaces[i].mIndices[j]);
-	  }
-	}
-
-	Mesh result;
-	result.vertices = all_vertices;
-	result.indices = all_indices;
-
-	load_vertex_bone_weights(mesh, result.vertices, model);
-	create_mesh(result);
-
-	return result;
-  }
-
-  static void load_vertex_bone_weights(const aiMesh *mesh, std::vector<AnimatedVertex> &vertices, Model &model) {
-	// NOTE: bone_index refers to a "relative index", that is, it is able to index into mesh->mBones.
-	// Which is NOT the same as bone_id, which is a globally unique id given to all bones.
-	for (int bone_index = 0; bone_index < (int)mesh->mNumBones; bone_index++) {
-	  int bone_id = -1;
-
-	  const auto bone_name = mesh->mBones[bone_index]->mName.C_Str();
-	  auto absolute_index = model.bone_name_to_index.find(bone_name);
-	  if (absolute_index == model.bone_name_to_index.end()) {
-		bone_id = model.next_bone_id;
-		// bone_index does not already exist in offset_matrix, so we create it & insert it
-		model.bone_offset_matrix.emplace_back(
-			Conversions::convertAssimpMat4ToGLM(mesh->mBones[bone_index]->mOffsetMatrix)
-		);
-		model.bone_name_to_index[bone_name] = bone_id;
-		model.next_bone_id += 1;
-	  } else {
-		// Otherwise, the bone already exists in the map so its ID is retrieved
-		bone_id = absolute_index->second;
-	  }
-
-	  // Configure the vertex data (weights and which bones impact this vertex)
-	  auto weights = mesh->mBones[bone_index]->mWeights;
-	  for (unsigned int weight_index = 0; weight_index < mesh->mBones[bone_index]->mNumWeights; weight_index++) {
-		// vertex_id is the index of the vertex that is influenced by the bone with bone_id
-		auto vertex_id = weights[weight_index].mVertexId;
-		vertices[vertex_id].set_weight_to_first_unset(bone_id, weights[weight_index].mWeight);
-	  }
-	}
-  }
-
-  // The animation consists of an array of channels.
-  // Each channel consists of keyframes, the channels also have a name that corresponds to the name of a node
-  // in the scene.
-  static void load_bones(Model &model, const aiAnimation *animation) {
-	for (unsigned int channel_index = 0; channel_index < animation->mNumChannels; channel_index++) {
-	  auto channel = animation->mChannels[channel_index];
-
-	  auto bone = model.bone_name_to_index.find(channel->mNodeName.data);
-	  if (bone == model.bone_name_to_index.end()) {
-		assert("This should not happen, something is wrong with the bone parsing");
-	  }
-
-	  model.bone_list.emplace_back(model.bone_name_to_index[channel->mNodeName.data], channel);
-	}
-  }
-
-  static void create_mesh(Mesh &mesh) {
-	glGenVertexArrays(1, &mesh.vao);
-	glGenBuffers(1, &mesh.vbo);
-	glGenBuffers(1, &mesh.ebo);
-
-	glBindVertexArray(mesh.vao);
-	glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
-	glBufferData(GL_ARRAY_BUFFER, mesh.vertices.size() * sizeof(AnimatedVertex), &mesh.vertices[0], GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-				 mesh.indices.size() * sizeof(unsigned int),
-				 &mesh.indices[0],
-				 GL_STATIC_DRAW);
-
-	// Vertex Positions
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(AnimatedVertex), nullptr);
-
-	// Bone IDs that affect this vertex
-	glEnableVertexAttribArray(1);
-	glVertexAttribIPointer(1, 4, GL_INT, sizeof(AnimatedVertex),
-						   (void *)offsetof(AnimatedVertex, bone_ids));
-
-	// Bone weights that affect this vertex
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(AnimatedVertex),
-						  (void *)offsetof(AnimatedVertex, bone_weights));
-
-	glBindVertexArray(0);
-  }
-
-  [[nodiscard]] static std::string get_base_path(const std::string &path) {
-	size_t pos = path.find_last_of("\\/");
-	return (std::string::npos == pos) ? "" : path.substr(0, pos + 1);
-  }
+  static void create_mesh(Mesh &mesh);
 };
 
 #endif //OPENGL_SKELETAL_ANIMATION_SRC_LOADERS_MODELLOADER_H_
--- src/models/animation/Bone.cpp
+++ src/models/animation/Bone.cpp
@@ -3,3 +3,68 @@
 //
 
 #include "Bone.h"
+Bone::Bone(int bone_id, aiNodeAnim *channel) {
+  this->bone_id = bone_id;
+  this->bone_name = channel->mNodeName.data;
+
+  // Convert all channel keyframes to our format
+  for (unsigned int pos_index = 0; pos_index < channel->mNumPositionKeys; pos_index++) {
+	auto keyframe = channel->mPositionKeys[pos_index];
+	position_keyframes.emplace_back(Conversions::convertAssimpVecToGLM(keyframe.mValue), keyframe.mTime);
+  }
+
+  for (unsigned int rot_index = 0; rot_index < channel->mNumRotationKeys; rot_index++) {
+	auto keyframe = channel->mRotationKeys[rot_index];
+	rotation_keyframes.emplace_back(Conversions::convertAssimpQuatToGLM(keyframe.mValue), keyframe.mTime);
+  }
+
+  for (unsigned int scale_index = 0; scale_index < channel->mNumScalingKeys; scale_index++) {
+	auto keyframe = channel->mScalingKeys[scale_index];
+	scale_keyframes.emplace_back(Conversions::convertAssimpVecToGLM(keyframe.mValue), keyframe.mTime);
+  }
+}
+void Bone::update_local_transformation(double animation_timestamp) {
+  auto current_pos = interpolate_vec3(position_keyframes, animation_timestamp);
+  auto current_rot = interpolate_quat(rotation_keyframes, animation_timestamp);
+  auto current_scale = interpolate_vec3(scale_keyframes, animation_timestamp);
+
+  local_transformation = glm::mat4(1.0f);
+  local_transformation = glm::translate(local_transformation, current_pos);
+  local_transformation *= glm::toMat4(current_rot);
+  local_transformation = glm::scale(local_transformation, current_scale);
+}
+glm::vec3 Bone::interpolate_vec3(const std::vector<Vec3KeyFrame> &keyframes, double timestamp) {
+  // If the size is 1, there is nothing to interpolate between
+  if (keyframes.size() == 1) {
+	return keyframes[0].vec;
+  }
+
+  unsigned int i = 0;
+  for (i = 0; i < keyframes.size() - 1; i++) {
+	if (keyframes[i + 1].timestamp > timestamp) {
+	  break;
+	}
+  }
+
+  auto mix = compute_mix(keyframes[i].timestamp, keyframes[i + 1].timestamp, timestamp);
+  return glm::mix(keyframes[i].vec, keyframes[i + 1].vec, mix);
+}
+glm::quat Bone::interpolate_quat(const std::vector<QuatKeyFrame> &keyframes, double timestamp) {
+  // If the size is 1, there is nothing to interpolate between
+  if (keyframes.size() == 1) {
+	return keyframes[0].quat;
+  }
+
+  unsigned int i = 0;
+  for (i = 0; i < keyframes.size() - 1; i++) {
+	if (keyframes[i + 1].timestamp > timestamp) {
+	  break;
+	}
+  }
+
+  auto mix = (float)compute_mix(keyframes[i].timestamp, keyframes[i + 1].timestamp, timestamp);
+  return glm::normalize(glm::slerp(keyframes[i].quat, keyframes[i + 1].quat, mix));
+}
+double Bone::compute_mix(double timestamp1, double timestamp2, double current_time) {
+  return (current_time - timestamp1) / (timestamp2 - timestamp1);
+}
